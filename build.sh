04670dcc1b916b81bd8af4f506d7e76c8cc459df
Martin Guillon <martin.guillon@akylas.fr>
2014-11-27 10:58:31 +0100

    updated build

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ IOSSDK_VER="7.0"
 # xcodebuild -showsdks
 PROJECT_NAME="SVGKit-iOS"
 TARGET_NAME=${PROJECT_NAME}
-xcodebuild -project ${PROJECT_NAME}.xcodeproj -target ${TARGET_NAME} -configuration Release -sdk iphoneos${IOSSDK_VER} build
-xcodebuild -project ${PROJECT_NAME}.xcodeproj -target ${TARGET_NAME} -configuration Release -sdk iphonesimulator${IOSSDK_VER} build
+xcodebuild -project ${PROJECT_NAME}.xcodeproj -target ${TARGET_NAME} -configuration Release -sdk iphoneos build
+xcodebuild -project ${PROJECT_NAME}.xcodeproj -target ${TARGET_NAME} -configuration Release -sdk iphonesimulator build
 
 cd Build
 
